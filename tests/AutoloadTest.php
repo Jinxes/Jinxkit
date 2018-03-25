0546be99e351d7e76a61fa8ce97b7e11ef8fd86b
jinxes <blldxt@yahoo.com>
2018-03-25 18:39:45 +0800

    remove test

--- tests/AutoloadTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Jinxes\Jinxkit\tests;
-
-spl_autoload_register(function ($class) {
-    $path = str_replace('Jinxes\\Jinxkit\\', '', $class);
-    $path = strtr($path, '\\', '/');
-    require $path . '.php';
-});
